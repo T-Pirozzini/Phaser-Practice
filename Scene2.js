62fa504dbe69ebb60f90a5a1de99609a7d9d1216
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-08-28 17:53:33 -0700

    add overlap physics to powerups, projectiles, enemies and player

--- Scene2.js
+++ Scene2.js
@@ -9,7 +9,13 @@ class Scene2 extends Phaser.Scene {
 
     this.ship1 = this.add.sprite(config.width / 2 - 50, config.height / 2, "ship");
     this.ship2 = this.add.sprite(config.width / 2, config.height / 2, "ship2");
-    this.ship3 = this.add.sprite(config.width / 2 + 50, config.height / 2, "ship3");    
+    this.ship3 = this.add.sprite(config.width / 2 + 50, config.height / 2, "ship3");  
+    
+    this.enemies = this.physics.add.group();
+    this.enemies.add(this.ship1);
+    this.enemies.add(this.ship2);
+    this.enemies.add(this.ship3);
+
     this.ship1.play("ship1_anim");
     this.ship2.play("ship2_anim");
     this.ship3.play("ship3_anim");
@@ -50,8 +56,16 @@ class Scene2 extends Phaser.Scene {
       powerUp.setCollideWorldBounds(true);
       powerUp.setBounce(1);
     }
-  }
 
+    this.physics.add.collider(this.projectiles, this.powerUps, function(projectile, powerUp) {
+      projectile.destroy();
+    });
+
+    this.physics.add.overlap(this.player, this.powerUps, this.pickPowerUp, null, this);
+    this.physics.add.overlap(this.player, this.enemies, this.hurtPlayer, null, this);
+    this.physics.add.overlap(this.projectiles, this.enemies, this.hitEnemy, null, this);
+  }  
+  
   moveShip(ship, speed) {
     ship.y += speed;
     if (ship.y > config.height) {
@@ -72,6 +86,20 @@ class Scene2 extends Phaser.Scene {
 
   shootBeam() {
     let beam = new Beam(this);
+  }
+
+  pickPowerUp(player, powerUp) {
+    powerUp.disableBody(true, true); // disable physics of powerUp, (True/True) make it inactive and hide it from display list
+  }
+
+  hurtPlayer(player, enemy) {
+    this.resetShipPos(enemy);
+    player.x = config.width / 2 - 8;
+    player.y = config.height - 64;
+  }
+  hitEnemy(projectile, enemy) {
+    projectile.destroy();    
+    this.resetShipPos(enemy)
   }
 
   update() {
